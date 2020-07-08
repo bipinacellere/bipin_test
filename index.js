a1b850ffc2d3face0c0b51abf8ee1f73069a7b84
Bipin Patel <bipin.patel@acellere.com>
2020-07-08 21:07:55 +0530

    Update index.js

--- index.js
+++ index.js
@@ -9,6 +9,6 @@ if (!req.body.lastName || req.body.lastName == "") {
     }
         elseif(1)
         {
-                console.log('Test message2');
+                console.log('Test message3');
                 return false;
         }
