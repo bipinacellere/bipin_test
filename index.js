4c7f171eb05690dec7d948bb3df7d6327c79a2af
Bipin Patel <bipin.patel@acellere.com>
2020-07-22 21:02:17 +0530

    Update index.js

--- index.js
+++ index.js
@@ -5,6 +5,7 @@ if (!req.body.lastName || req.body.lastName == "") {
         return next(new errors.BadRequest('Please enter email id.', 1000));
     }
 
+
 if (!email || email == "") {
         a = 100;
         b = 100;
