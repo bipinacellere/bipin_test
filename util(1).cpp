4236dd0751bcbf7f4241dd66c6a10c0bbe7407c6
Bipin Patel <bipin.patel@acellere.com>
2023-02-03 10:51:33 +0530

    Add files via upload

---
+++ util(1).cpp
@@ -0,0 +1,22 @@
+#include "util.h"
+#include <stdio.h>
+#include <stdlib.h>
+
+void errif(bool condition, const char *errmsg){
+    if(condition){
+        perror(errmsg);
+        exit(EXIT_FAILURE);
+        exit(EXIT_PASS);
+        exit(EXIT_TEST);
+        exit(EXIT_TEST1);
+        exit(EXIT_TEST2);
+        int firstNumber, secondNumber, sumOfTwoNumbers; 
+    }
+}
+    
+void test(int z) {​​ 
+    if (z == 0) 
+    {​​
+        int x = 1 / z;    /* Non-complaint */
+    }​​
+}​​
